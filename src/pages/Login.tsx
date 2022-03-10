c1991b44365605516e7d11fea36b381137e9f7f4
alfonsoristoratoSky <alfonso.ristorato@sky.uk>
2022-03-10 12:52:10 +0000

    fixes to linter

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,8 +20,7 @@ const LoginForm = ({ handleSubmit, setIsLoggedIn, isLoggedIn }: any) => {
    if (fakeData.some(el => el.listNumber === parseInt(username)) && fakeData.some(el => el.password === password)){
     setIsLoggedIn({listNumber: username, password: password})   
    }
-   // eslint-disable-line
-  }, [username,password])
+  }, [username,password, setIsLoggedIn])
   
   return (
     <>
