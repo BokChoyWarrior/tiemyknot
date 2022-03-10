2eb0ef2b57e0a81660d2506e4f719b9b03feb6ac
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2022-03-10 14:38:29 +0000

    Make prettier only show warnings

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
 import { fakeData } from '../fakeData/fakeData';
 import '../styles/Login.css';
-import {Navigate} from 'react-router-dom'
-export function Login({isLoggedIn, setIsLoggedIn} : any) { 
-  
-  return (
-    isLoggedIn ?  <Navigate to='/gifts'/> :
+import { Navigate } from 'react-router-dom';
+export function Login({ isLoggedIn, setIsLoggedIn }: any) {
+  return isLoggedIn ? (
+    <Navigate to="/gifts" />
+  ) : (
     <div className="container">
       <h1>Access a wedding list</h1>
       <LoginForm setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn}></LoginForm>
@@ -17,18 +17,21 @@ const LoginForm = ({ handleSubmit, setIsLoggedIn, isLoggedIn }: any) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   useEffect(() => {
-   if (fakeData.some(el => el.listNumber === parseInt(username)) && fakeData.some(el => el.password === password)){
-    setIsLoggedIn({listNumber: username, password: password})   
-   }
-  }, [username,password])
-  
+    if (
+      fakeData.some((el) => el.listNumber === parseInt(username)) &&
+      fakeData.some((el) => el.password === password)
+    ) {
+      setIsLoggedIn({ listNumber: username, password: password });
+    }
+  }, [username, password]);
+
   return (
     <>
       <FormField label="Wedding code" type="text" data={username} setData={setUsername}></FormField>
       <FormField label="Password" type="password" data={password} setData={setPassword}></FormField>
     </>
   );
-};         
+};
 
 const FormField = ({ label, type, hidden = false, data, setData }: any) => {
   return (
