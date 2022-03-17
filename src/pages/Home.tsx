1bc158bb4c9b0037659ae7204e54448b3116b7c2
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2022-03-17 15:07:53 +0000

    Add aria labels and image descriptions for icons
    Co-authored-by: Myles Gordon <mylesgordon@users.noreply.github.com>
    Co-authored-by: alfonsoristoratoSky <alfonsoristoratoSky@users.noreply.github.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,23 +7,22 @@ export function Home(props: any) {
   return (
     <div id="homeContainer">
       <div className="buttonWithIcon">
-        <span className="material-icons large-icon">add_circle_outline</span>
+        <span className="material-icons large-icon" aria-label="create new list icon">
+          add_circle_outline
+        </span>
         <button className="blueButton" onClick={() => navigate('/123')}>
           Create new wedding list
         </button>
       </div>
 
       <div className="buttonWithIcon">
-        <span className="material-icons large-icon">checklist_rtl</span>
+        <span className="material-icons large-icon" aria-label="checklist icon">
+          checklist_rtl
+        </span>
         <button className="blueButton" onClick={() => navigate('/access')}>
-          {props.isLoggedIn ? `Access list number ${props.isLoggedIn.listNumber}` : "Access a wedding list"}
+          {props.isLoggedIn ? `Access list number ${props.isLoggedIn.listNumber}` : 'Access a wedding list'}
         </button>
-       
       </div>
-
-      
-       
-     
     </div>
   );
 }
